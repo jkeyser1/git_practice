dfe8f18acccbce00cf975833a9e30beb648d8802
jkeyser <jkeyser@crooz.co.jp>
2015-12-03 18:55:00 +0900

    Age Test +1 from Chapter 2

---
+++ 1203AgeTest.java
@@ -0,0 +1,10 @@
+
+public class Main { 
+  public static void main(String[] args){
+     String age = "23";
+     int n = Integer.parseInt (age);
+     System.out.println
+	            ("Next year, you will be"   + (n + 1) +   "years old!");
+  }
+ }
+
