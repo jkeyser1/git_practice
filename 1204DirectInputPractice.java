92787cd366e61742b1720b27c7021286409e0c17
jkeyser <jkeyser@crooz.co.jp>
2015-12-05 01:43:09 +0900

    1204 User Input Practice

---
+++ 1204DirectInputPractice.java
@@ -0,0 +1,15 @@
+public class Main { 
+  public static void main(String[] args) {
+    //Query for user input
+    System.out.println("Please enter your name");
+    //User input section
+    String name = new java.util.Scanner(System.in).nextLine();
+    //Query for user age
+    System.out.println("Please enter your age");
+    //Age input
+    int age = new java.util.Scanner(System.in).nextInt();
+    // Text display welcoming the user
+    System.out.println ("Welcome, Mr./Mrs." + name + "who is" + age + "years old.");
+					      
+					        }
+						
