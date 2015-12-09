8afc81aea11cbe869dd7210c990fffcf26cb1571
jkeyser1 <jkeyser@crooz.co.jp>
2015-12-09 23:08:12 +0900

    Create 1202ComparisonTest.java

---
+++ 1202ComparisonTest.java
@@ -0,0 +1,10 @@
+
+public class Main { 
+  public static void main(String[] args){
+   int a = 5;
+   int b = 3;
+   int m = Math.max(a,b);
+    System.out.println("Comparison test:" +
+	                a + "and" +  b + "and the larger number is" +  m);
+  }
+ }
